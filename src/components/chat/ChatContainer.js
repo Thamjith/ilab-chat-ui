d46b03f021c4a57fc3bad8f2aa37535b7ac72c25
Thamjith Thaha <thamjith.thaha@ibm.com>
2024-07-17 18:31:38 +0530

    initial questionair

--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -1,15 +1,17 @@
-import React from 'react';
 import './ChatContainer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import ChatBox from './ChatBox';
 
-const ChatContainer = ({ responses, message, setMessage, handleSendMessage, handleKeyDown, isSending }) => {
+import ChatBox from './ChatBox';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Onboarding from '../onboarding/Onboarding';
+import React from 'react';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+const ChatContainer = ({ responses, message, setMessage, handleSendMessage, handleKeyDown, isSending, InitialQuestion }) => {
   const sendMessage = () => {
     if (!message.trim() || isSending) return;
     handleSendMessage(message);
   };
-
+// console.log("initialQuestion = ", initialQuestion)
   return (
     <div className="chat-container" style={{
       backgroundImage: `url(${process.env.PUBLIC_URL}/chatbot.png)`,
@@ -19,14 +21,18 @@ const ChatContainer = ({ responses, message, setMessage, handleSendMessage, hand
     }}>
       <ChatBox responses={responses} />
       <div className="input-box">
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type your message..."
-          onKeyDown={handleKeyDown}
-          disabled={isSending}
-        />
+        {
+            InitialQuestion ?  
+              <Onboarding setMessage={setMessage} /> :
+              <input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Type your message..."
+                onKeyDown={handleKeyDown}
+                disabled={isSending}
+              />
+          }
         <button onClick={sendMessage} disabled={isSending}>
           <FontAwesomeIcon icon={faPaperPlane} />
         </button>
---
+++ src/components/onboarding/Onboarding.jsx
@@ -0,0 +1,100 @@
+import "./Onboarding.css"
+
+import React, { useEffect, useState } from 'react'
+
+const createSelectedOptionsString = (selectedOptions) => {
+    return Object.keys(selectedOptions)
+      .filter(key => selectedOptions[key])
+      .join(', ');
+  };
+
+const Onboarding = ({setMessage}) => {
+    const [selectedOptions, setSelectedOptions] = useState({
+        agent_deployment: false,
+        application_perspective: false,
+        website_and_mobile_app_monitoring: false,
+        custom_dashboards: false,
+        synthetic_monitoring: false
+    });
+
+    const handleCheckboxChange = (event) => {
+        const { name, checked } = event.target;
+        setSelectedOptions({
+            ...selectedOptions,
+            [name]: checked
+        });
+    };
+
+    useEffect(() => {
+        setMessage(createSelectedOptionsString(selectedOptions))
+    }, [selectedOptions])
+
+  return (
+    <div className='questionaire'>
+        <h5>
+            Hi! Welcome to Instana. I will help you onboard to Instana. Please select one of the goals from the list below:
+        </h5>
+
+        <div className="checkbox-container">
+            <div>
+                <input 
+                    type="checkbox" 
+                    id="agent_deployment" 
+                    name="agent_deployment" 
+                    value="Agent deployment" 
+                    checked={selectedOptions.agent_deployment} 
+                    onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="agent_deployment">Agent deployment</label>
+            </div>
+            <div>
+                <input 
+                    type="checkbox" 
+                    id="application_perspective" 
+                    name="application_perspective" 
+                    value="Application perspective" 
+                    checked={selectedOptions.application_perspective} 
+                    onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="application_perspective">Application perspective</label>
+            </div>
+            <div>
+                <input 
+                    type="checkbox" 
+                    id="website_and_mobile_app_monitoring" 
+                    name="website_and_mobile_app_monitoring" 
+                    value="Website and Mobile App monitoring" 
+                    checked={selectedOptions.website_and_mobile_app_monitoring} 
+                    onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="website_and_mobile_app_monitoring">Website and Mobile App monitoring</label>
+            </div>
+            <div>
+                <input 
+                    type="checkbox" 
+                    id="custom_dashboards" 
+                    name="custom_dashboards" 
+                    value="Custom Dashboards" 
+                    checked={selectedOptions.custom_dashboards} 
+                    onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="custom_dashboards">Custom Dashboards</label>
+            </div>
+            <div>
+                <input 
+                    type="checkbox" 
+                    id="synthetic_monitoring" 
+                    name="synthetic_monitoring" 
+                    value="Synthetic monitoring" 
+                    checked={selectedOptions.synthetic_monitoring} 
+                    onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="synthetic_monitoring">Synthetic monitoring</label>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Onboarding
